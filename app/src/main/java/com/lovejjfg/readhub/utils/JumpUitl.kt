3fba337a800e91215ec74c7ceabe7b337fb13be6
joe <lovejjfg@gmail.com>
2017-12-06 12:32:44 +0800

    add try catch in web jump

--- app/src/main/java/com/lovejjfg/readhub/utils/JumpUitl.kt
+++ app/src/main/java/com/lovejjfg/readhub/utils/JumpUitl.kt
@@ -26,8 +26,8 @@ import android.preference.PreferenceManager
 import android.text.TextUtils
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.lovejjfg.readhub.data.Constants
-import com.lovejjfg.readhub.data.topic.Extra
 import com.lovejjfg.readhub.view.*
+import com.tencent.bugly.crashreport.CrashReport
 
 
 /**
@@ -38,24 +38,28 @@ import com.lovejjfg.readhub.view.*
 object JumpUitl {
 
     fun jumpWeb(context: Context, url: String?) {
-        if (TextUtils.isEmpty(url)) {
-            return
-        }
-        val bundle = Bundle()
-        bundle.putString("链接", url)
-        FirebaseAnalytics.getInstance(context).logEvent("点击", bundle)
-        val default = PreferenceManager
-                .getDefaultSharedPreferences(context)
-                .getBoolean("browser_use", false)
+        try {
+            if (TextUtils.isEmpty(url)) {
+                return
+            }
+            val bundle = Bundle()
+            bundle.putString("链接", url)
+            FirebaseAnalytics.getInstance(context).logEvent("点击", bundle)
+            val default = PreferenceManager
+                    .getDefaultSharedPreferences(context)
+                    .getBoolean("browser_use", false)
 
-        if (!default) {
-            val intent = Intent(context, WebActivity::class.java)
-            intent.putExtra(Constants.URL, url)
-            context.startActivity(intent)
-        } else {
-            val uri = Uri.parse(url)
-            val intent = Intent(Intent.ACTION_VIEW, uri)
-            context.startActivity(intent)
+            if (!default) {
+                val intent = Intent(context, WebActivity::class.java)
+                intent.putExtra(Constants.URL, url)
+                context.startActivity(intent)
+            } else {
+                val uri = Uri.parse(url)
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                context.startActivity(intent)
+            }
+        } catch (e: Exception) {
+            CrashReport.postCatchedException(e)
         }
     }
 
